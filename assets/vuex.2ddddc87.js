035e80cdebb2209f15bf7519d51b84a037e22701
fantasy0310 <fantasy0310@users.noreply.github.com>
2021-04-30 08:23:05 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter af4b9d46dacfdc6dc8004d5ecc77fd6d79d4a693

---
+++ assets/vuex.2ddddc87.js
@@ -0,0 +1,1 @@
+import{d as e,g as r,o as n,c as o}from"./vendor.8608ac3e.js";var t=e({name:"Home",props:{msg:{type:String,required:!0}},setup:()=>({count:r(0)})});t.render=function(e,r,t,a,s,u){return n(),o("div",null,"vuex")};export default t;
--- assets/vuex.2f583368.js
+++
@@ -1,1 +0,0 @@
-import{d as e,g as r,o as n,c as o}from"./vendor.110ee10a.js";var t=e({name:"Home",props:{msg:{type:String,required:!0}},setup:()=>({count:r(0)})});t.render=function(e,r,t,a,s,u){return n(),o("div",null,"vuex")};export default t;
